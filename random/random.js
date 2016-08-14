17019840a14c3fec44bc9e54f5c43bc694eb0fe4
Steve Dien <lulzsecd@gmail.com>
2016-08-14 17:52:35 +0700

    Đáp án bài web đánh đê

---
+++ random/random.js
@@ -0,0 +1,21 @@
+function quay(){
+    return Math.floor(Math.random() * 100);
+}
+btn.onclick = function(){
+    var de = parseInt(sode.value);
+    if(isNaN(de)){
+        ketqua.innerHTML = "Bạn chưa nhập gì cả";
+    } else {
+        if(de == quay()){
+            ketqua.innerHTML = "Xin chúc mừng! Bạn đã trúng đề :)";
+        } else {
+            ketqua.innerHTML = "Chúc bạn may mắn lần sau! :(";
+        }
+        setTimeout(function () {
+            btn.innerHTML = "Quay lại";
+            ketqua.innerHTML = "Click để quay lại";
+        }, 2000);
+    }
+
+};
+
